7c9fd056dd55332c45db12312b0648c2e2b03e83
Clifford Mapesa <cliffordmapesa8@gmail.com>
2022-06-27 09:42:47 +0300

    additional data

--- js/fin-settings.js
+++ js/fin-settings.js
@@ -14,6 +14,22 @@ firebase.auth().onAuthStateChanged((user)=>{
             })
         }
 
+
+        //add payent cycle
+        document.getElementById("saveCycle").onclick = function(){
+            let cyclename = document.getElementById("cyclename").value;
+
+            let addCycle = firebase.firestore().collection("paymentCycle").doc();
+            addCycle.set({
+
+                cyclename:cyclename,
+                docId:addCycle.id
+
+            }).then(()=>{
+                window.location.reload();
+            })
+        }
+
         //view payment method
         firebase.firestore().collection("paymentMethod").get().then((querySnapshot)=>{
             let content = '';
@@ -23,12 +39,29 @@ firebase.auth().onAuthStateChanged((user)=>{
                 let docId = doc.data().docId;
 
                 content+=    '<tr>';
-                content+=    '<td>' + pMethod +'</td>';
-                content+=    '<td> <button class="btn btn-danger">Delete</button> </td>';                    
+                content+=    '  <td> ' + pMethod +'</td>';
+                content+=    '  <td> <button class="btn btn-danger">Delete</button> </td>';                    
                 content+=    '</tr>';
 
             })
             $("#paymentMethodList").append(content);
+        })
+
+        //view payment cycle
+        firebase.firestore().collection("paymentCycle").get().then((querySnapshot)=>{
+            let content = '';
+            querySnapshot.forEach((doc)=>{
+
+                let cyclename = doc.data().cyclename;
+                let docId = doc.data().docId;
+
+                content+=    '<tr>';
+                content+=    '  <td> ' + cyclename +'</td>';
+                content+=    '  <td> <button class="btn btn-danger">Delete</button> </td>';                    
+                content+=    '</tr>';
+
+            })
+            $("#paymentCycleList").append(content);
         })
 
     }else{
